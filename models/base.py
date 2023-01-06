594fb268a30bf9b9cd405eb8b27a49200445b547
shenmishajing <shenmishajing@gmail.com>
2023-01-06 19:11:58 +0800

    use loss_weights and loss_modules instead of loss_config, remove loss_use_loss_weight feature

--- models/base.py
+++ models/base.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Any, Mapping, Union
+from typing import Mapping
 
 import torch
 from lightning.pytorch import LightningModule as _LightningModule
@@ -10,28 +10,25 @@ class LightningModule(_LightningModule, BaseModule, ABC):
     def __init__(
         self,
         normalize_config=None,
-        loss_config: Mapping[
-            str,
-            Union[torch.nn.Module, Mapping[str, Union[torch.nn.Module, int, float]]],
-        ] = None,
-        *args: Any,
-        **kwargs: Any,
+        loss_weights=None,
+        loss_modules: Mapping[str, torch.nn.Module] = None,
+        *args,
+        **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
         self.automatic_lr_schedule = True
         self.normalize_config = normalize_config
         self.lr = None
         self.batch_size = None
-
-        if loss_config is not None:
-            self._parse_loss_config(loss_config)
+        self.loss_weights = loss_weights
+        self.loss_modules = torch.nn.ModuleDict(loss_modules if loss_modules else {})
 
     def configure_optimizer_parameters(self):
         return None
 
     @staticmethod
-    def add_prefix(log_dict, prefix="train/"):
-        return {f"{prefix}{k}": v for k, v in log_dict.items()}
+    def add_prefix(log_dict, prefix="train", sep="/"):
+        return {f"{prefix}{sep}{k}": v for k, v in log_dict.items()}
 
     def log(self, *args, batch_size=None, **kwargs):
         if (
@@ -42,50 +39,24 @@ class LightningModule(_LightningModule, BaseModule, ABC):
             batch_size = self.batch_size
         super().log(*args, batch_size=batch_size, **kwargs)
 
-    def _parse_loss_config(self, loss_config):
-        for key, value in loss_config.items():
-            if not isinstance(value, Mapping):
-                loss_config[key] = {"module": value, "weight": 1}
-            setattr(self, "loss_" + key, loss_config[key]["module"])
-        self.loss_weight = {k: v.get("weight", 1) for k, v in loss_config.items()}
-
-    def _loss_step(self, batch, res, prefix="train"):
+    def _loss_step(self, batch, res):
         raise NotImplementedError
 
     def loss_step(
         self,
         batch,
         res,
-        prefix="train",
         use_loss_weight=True,
-        loss_use_loss_weight=True,
-        detach=None,
     ):
-        loss = self._loss_step(batch, res, prefix)
+        loss = self._loss_step(batch, res)
         # multi loss weights
-        if use_loss_weight:
+        if use_loss_weight and self.loss_weights:
             loss = {
-                k: v * (1 if k not in self.loss_weight else self.loss_weight[k])
+                k: v * (1 if k not in self.loss_weights else self.loss_weights[k])
                 for k, v in loss.items()
             }
         # calculate loss
-        if not use_loss_weight and loss_use_loss_weight:
-            total_loss = [
-                v * (1 if k not in self.loss_weight else self.loss_weight[k])
-                for k, v in loss.items()
-            ]
-        else:
-            total_loss = [v for k, v in loss.items()]
-        loss["loss"] = torch.sum(torch.stack(total_loss))
-        # add prefix
-        if detach is None:
-            detach = prefix != "train"
-        loss = {
-            (f"{prefix}/" if prefix is not None else "")
-            + ("loss_" if "loss" not in k else "")
-            + k: (v.detach() if detach else v)
-            for k, v in loss.items()
-        }
+        loss["loss"] = torch.sum(torch.stack(list(loss.values())))
         return loss
 
     def on_fit_start(self):
@@ -96,18 +67,18 @@ class LightningModule(_LightningModule, BaseModule, ABC):
 
     def training_step(self, batch, *args, **kwargs):
         res = self(batch)
-        loss = self.loss_step(batch, res, "train")
-        self.log_dict(loss)
-        return loss["train/loss"]
+        loss_dict = self.loss_step(batch, res)
+        self.log_dict(self.add_prefix(loss_dict, prefix="train"))
+        return loss_dict
 
     def validation_step(self, batch, *args, **kwargs):
         res = self(batch)
-        loss = self.loss_step(batch, res, "val")
-        self.log_dict(loss, sync_dist=True)
-        return loss
+        loss_dict = self.loss_step(batch, res)
+        self.log_dict(self.add_prefix(loss_dict, prefix="val"), sync_dist=True)
+        return loss_dict
 
     def test_step(self, batch, *args, **kwargs):
         res = self(batch)
-        loss = self.loss_step(batch, res, "test")
-        self.log_dict(loss, sync_dist=True)
-        return loss
+        loss_dict = self.loss_step(batch, res)
+        self.log_dict(self.add_prefix(loss_dict, prefix="test"), sync_dist=True)
+        return loss_dict
